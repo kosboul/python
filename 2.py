cb80361d4bd04120a0a81a6b3afd30bde32266fc
kosboul <21040553+kosboul@users.noreply.github.com>
2022-01-15 21:18:07 -0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,3 +2,5 @@ a = 1
 _a = 2
 _a2 = 3
 2a = 4
+
+# Initially thought than an underscore was an illegal character for a variable, but was wrong. I did correctly identify that 2a cannot be a variable name
