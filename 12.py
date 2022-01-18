1801eddf0d67998c9790927bf36efd5b88ae3721
kosboul <21040553+kosboul@users.noreply.github.com>
2022-01-17 22:14:24 -0500

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,7 @@
+# Complete the script so that it produces the expected output. Please use my_range  as input data
+# Expected output: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
+
+
+my_range = range(10,200,10)
+
+print(list(my_range))
