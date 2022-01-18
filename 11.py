fea313fe71f2d2514db6c1a346fe158e5c6e9bf9
kosboul <21040553+kosboul@users.noreply.github.com>
2022-01-17 22:06:42 -0500

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,6 +1,9 @@
 # Create a list from 1 to 20, but noy manually
 # Hint: Take a look at the range function
 
-x = [range(1,21)]
+# x = [range(1,21)]
 
-print(x)
+# print(x)
+
+my_range = range(1, 21)
+print(list(my_range))
