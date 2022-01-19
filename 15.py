2e2297d816d010e53e99ccbc9b414f0b1b30af15
kosboul <21040553+kosboul@users.noreply.github.com>
2022-01-18 21:52:03 -0500

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,4 +1,4 @@
 # Create a dictionary that contains the keys a  and b  and their respective values 1 and 2
 # Hint: Dictionaries can be created either by using curly brackets or by using the dict function
 
-my_dictionary = {"a":1, "b":2}
+my_dictionary = {a:1, b:2}
